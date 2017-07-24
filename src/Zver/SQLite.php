d9c844dd43fdbda6530a41c234b2d15837d0a338
s4urp8n <s4urp8n@yandex.ru>
2017-07-24 23:37:52 +0300

    initial commit

--- src/Zver/SQLite.php
+++ src/Zver/SQLite.php
@@ -7,36 +7,81 @@ namespace Zver {
 
         protected $connection = null;
 
-        public function connect($databasePath, $login = null, $password = null, $persistent = false)
+        public static function connect($databasePath, $login = null, $password = null, $persistent = false)
         {
 
             /**
              * Create file if it not exists
              */
             clearstatcache(true);
+
             if (!file_exists($databasePath)) {
                 Common::createDirectoryIfNotExists(dirname($databasePath));
                 touch($databasePath);
             }
 
-            /**
-             * Try to connect
-             */
-            if (is_null($this->connection)) {
+            return new static($databasePath, $login, $password, $persistent);
+        }
 
-                $pdo = new \PDO('sqlite:' . $databasePath, $login, $password, [
-                    \PDO::ATTR_PERSISTENT => $persistent,
-                    \PDO::ATTR_ERRMODE    => \PDO::ERRMODE_EXCEPTION,
-                ]);
-
-                if (!is_resource($pdo)) {
-                    throw new \Exception("Cant connect to db " . $databasePath);
-                }
-            }
+        public function __construct($databasePath, $login, $password, $persistent)
+        {
+            $pdo = new \PDO('sqlite:' . $databasePath, $login, $password, [
+                \PDO::ATTR_PERSISTENT => $persistent,
+                \PDO::ATTR_ERRMODE    => \PDO::ERRMODE_EXCEPTION,
+            ]);
 
             $this->connection = $pdo;
+        }
 
-            return $this->connection;
+        public function insertInTransaction($table, array $values, $ignore = false)
+        {
+
+            $this->execute("PRAGMA synchronous=OFF");
+            $this->execute("PRAGMA count_changes=OFF");
+            $this->execute("PRAGMA journal_mode=NORMAL");
+            $this->execute("PRAGMA temp_store=MEMORY");
+
+            $this->connection->beginTransaction();
+
+            foreach ($values as $value) {
+                $this->insert($table, $value, $ignore);
+            }
+
+            $this->connection->commit();
+
+        }
+
+        public function fetch($query)
+        {
+            return $this->connection->query($query)
+                                    ->fetchAll(\PDO::FETCH_ASSOC);
+        }
+
+        public function insert($table, array $values, $ignore = false)
+        {
+
+            $insertFields = $insertValues = [];
+
+            foreach ($values as $key => $value) {
+
+                $insertFields[] = $this->escape($key);
+                $insertValues[] = $this->escape($value);
+
+            }
+
+            $query = sprintf(
+                'insert' . ($ignore ? ' or ignore ' : ' ') . 'into `' . $table . '` (%s) values (%s)',
+                implode(',', $insertFields),
+                implode(',', $insertValues)
+            );
+
+            return $this->execute($query);
+
+        }
+
+        public function __destruct()
+        {
+            $this->disconnect();
         }
 
         public function disconnect()
@@ -48,7 +93,12 @@ namespace Zver {
 
         public function execute($query)
         {
+            return $this->connection->exec($query);
+        }
 
+        public function escape($string)
+        {
+            return $this->connection->quote($string);
         }
 
     }
--- tests/SQLiteTest.php
+++ tests/SQLiteTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Zver\SQLite;
+
 class SQLiteTest extends PHPUnit\Framework\TestCase
 {
 
@@ -15,9 +17,62 @@ class SQLiteTest extends PHPUnit\Framework\TestCase
 
     }
 
+    public function getTestDB()
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'test.db';
+    }
+
     public function testConnectAndCreate()
     {
-        
+        $this->assertFalse(empty(SQLite::connect($this->getTestDB())));
+    }
+
+    public function testExecuteAndInsert()
+    {
+        $sq = SQLite::connect($this->getTestDB());
+        $this->assertFalse(empty(SQLite::connect($this->getTestDB())));
+
+        $result = $sq->execute("drop table if EXISTS  testtable");
+        $result = $sq->execute("create table if not EXISTS  testtable ( 'id' integer(10), 'name' char(32) )");
+
+        $values = [];
+
+        for ($i = 0; $i < 100; $i++) {
+
+            $result = $sq->insert('testtable', [
+                'id'   => rand(9, 9999),
+                'name' => md5(rand(9, 9999)),
+            ]);
+
+            $values[] = [
+                'id'   => rand(9, 9999),
+                'name' => md5(rand(9, 9999)),
+            ];
+        }
+
+        $sq->insertInTransaction('testtable', $values, false);
+
+        /**
+         * Select count and data
+         */
+
+        $data = $sq->fetch('select * from testtable');
+
+        $this->assertTrue(count($data) == 200);
+
+        $result = $sq->execute("drop table if EXISTS  testtable");
+        $result = $sq->execute("vacuum");
+
+        $sq->disconnect();
+
+        @unlink($this->getTestDB());
+    }
+
+    public function testDisconnect()
+    {
+        $sq = SQLite::connect($this->getTestDB());
+        $sq->disconnect();
+        $this->assertTrue(true);
     }
 
 }
